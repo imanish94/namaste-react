40ee3f1817057e7ecb6248ef09b7a814f7174635
Manish Sharma <imanish.indev@gmail.com>
2026-07-31 19:08:12 +0530

    added : percel, react, .gitingnore and packages.json

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-const create = React.createElement('div', {className: 'container'}, [
-    React.createElement('div', {className: 'row'}, [
-        React.createElement('div', {className: 'col'}, [
-            React.createElement('h1', {id: 'heading'}, 'Namaste React Akshay Saini')
-        ])
-    ])
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const create = React.createElement("div", { className: "container" }, [
+  React.createElement("div", { className: "row" }, [
+    React.createElement("div", { className: "col" }, [
+      React.createElement(
+        "h1",
+        { id: "heading" },
+        "Namaste React Akshay Saini",
+      ),
+    ]),
+  ]),
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(create);
